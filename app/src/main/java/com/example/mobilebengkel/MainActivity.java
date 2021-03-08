573a3f5a33f59b1a769c47812f81405ca090f1ec
Muhammad Nurwidya Ardiansyah <muhammadnurwidya.2019@student.uny.ac.id>
2021-03-08 22:57:28 +0700

    membuat layout landing, login, register

--- app/src/main/java/com/example/mobilebengkel/MainActivity.java
+++ app/src/main/java/com/example/mobilebengkel/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.mobilebengkel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int timeout = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing);
+        final View myLayout = findViewById(R.id.startscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent i = new Intent(MainActivity.this, ActivityTwo.class);
+                startActivity(i);
+                finish();
+            }
+        },timeout);
     }
 }
